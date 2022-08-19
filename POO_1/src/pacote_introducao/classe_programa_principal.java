e5f507b008224e20b895c8a9a31f23451f3d4ed4
joaomarcosmartinsdesouza <84867094+joaomarcosmartinsdesouza@users.noreply.github.com>
2022-08-19 19:48:53 -0300

    Adicionadas linhas no codigo, corrigida pontuacao e removidos acentos nas palavras.

--- POO_1/src/pacote_introducao/classe_programa_principal.java
+++ POO_1/src/pacote_introducao/classe_programa_principal.java
@@ -5,7 +5,10 @@ public class classe_programa_principal {
 	public static void main(String[] args) {
 		
 		System.out.println("Primeiro programa java");
-		System.out.println("Criando primeiro repositório no Github a partir da IDE Eclipse");
+		System.out.println("Criando primeiro repositorio no Github a partir da IDE Eclipse.");
+		System.out.println("Acrescentando tres linhas no codigo para verificar novo Commit no GitHub.");
+		System.out.println("Observou?");
+		System.out.println("Removi acentos das palavras repositorio e codigo.");
 	}
 
 }
