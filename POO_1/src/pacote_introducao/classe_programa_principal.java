6d01373f09551a3915065379b0d73f1649f234bf
João Marcos Martins de Souza <84867094+joaomarcosmartinsdesouza@users.noreply.github.com>
2022-08-19 19:08:12 -0300

    Adicionei arquivos via upload (arrastei pasta do explorer para área do GitHub

---
+++ POO_1/src/pacote_introducao/classe_programa_principal.java
@@ -0,0 +1,11 @@
+package pacote_introducao;
+
+public class classe_programa_principal {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Primeiro programa java");
+		System.out.println("Criando primeiro repositório no Github a partir da IDE Eclipse");
+	}
+
+}
