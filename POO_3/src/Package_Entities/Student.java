d390f953126d55cd2c3318108afee29093931ac8
João Marcos Martins de Souza <84867094+joaomarcosmartinsdesouza@users.noreply.github.com>
2022-08-20 21:27:48 -0300

    Added file named POO_3 via upload

---
+++ POO_3/src/Package_Entities/Student.java
@@ -0,0 +1,20 @@
+package Package_Entities;
+
+public class Student {         // Class name
+	
+	public String nome;        // Class attributes
+	public int nota1;
+	public int nota2;
+	public float media;
+	
+public float mediaAluno() {                // Class method number 1 
+	float mediaAluno = (nota1 + nota2)/2;
+	return mediaAluno;
+}
+public void verificaAluno() {              // Class method number 2
+	if (media>=60)
+		System.out.println(nome+" estah aprovado");
+	else
+		System.out.println(nome+" estah reprovado");
+	}
+}
